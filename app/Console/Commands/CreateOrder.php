b230a15c88ac8608462defe2b6cf1ab77d95f40b
Przełożny <kacper.przelozny@fingoweb.com>
2024-05-16 20:07:55 +0200

    change columns, add command

---
+++ app/Console/Commands/CreateOrder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class CreateOrder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'order:create {period_id} {item_id} {amount}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $order = Order::query()->create([
+            'period_id' => $this->argument('period_id'),
+            'item_id' => $this->argument('item_id'),
+            'amount' => $this->argument('amount'),
+            'order_date' => Carbon::now(),
+        ]);
+
+        print_r($order);
+    }
+}
